f098d3f3ea0fa87877d20a13d1e6d2152d8919b9
ATOMPassakorn <atom15440@gmail.com>
2025-01-31 14:04:09 +0700

    Life Is DSA

---
+++ Labs 05.01.py
@@ -0,0 +1,6 @@
+"""O(n)"""
+def suMmation(n):
+    for i in range(n):
+        n+=i
+    print(n)
+suMmation(int(input()))
---
+++ Labs 05.02.py
@@ -0,0 +1,5 @@
+"""O(1)"""
+def suMmation(n):
+    result=(n*(n+1))/2
+    print(int(result))
+suMmation(int(input()))
